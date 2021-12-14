565a75ec83792989f9291b5d4c65ad3320ab5a44
Kutay Sütçü <94748829+SutcuKutay@users.noreply.github.com>
2021-12-14 16:39:54 +0300

    Proje oluşturuldu.

---
+++ WebProgramlamaProjesi/WebProgramlamaProjesi/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WebProgramlamaProjesi.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
