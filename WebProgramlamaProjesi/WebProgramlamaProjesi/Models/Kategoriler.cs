edfe79b22ee1ab05e91e7cd3c4bd08765633b118
ibrahimatif <62612359+ibrahimatif@users.noreply.github.com>
2021-12-14 17:23:41 +0300

    model üzerinde çalışıldı

---
+++ WebProgramlamaProjesi/WebProgramlamaProjesi/Models/Kategoriler.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebProgramlamaProjesi.Models
+{
+    public class Kategoriler
+    {
+        public int KategoriId { get; set; }
+
+        public string KategoriAd { get; set; }
+
+
+    }
+}
---
+++ WebProgramlamaProjesi/WebProgramlamaProjesi/Models/Urun.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebProgramlamaProjesi.Models
+{
+    public class Urun
+    {
+        public int UrunId { get; set; }
+
+        public string Ad { get; set; }
+
+        public int Fiyat { get; set; }
+
+
+    }
+}
