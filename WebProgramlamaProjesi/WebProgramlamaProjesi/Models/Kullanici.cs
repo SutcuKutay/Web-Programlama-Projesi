533f1388860dfc8583476107150f653ef791cb0d
Kutay Sütçü <94748829+SutcuKutay@users.noreply.github.com>
2021-12-15 23:39:34 +0300

    Kullanici modeli eklendi

---
+++ WebProgramlamaProjesi/WebProgramlamaProjesi/Models/Kullanici.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebProgramlamaProjesi.Models
+{
+    public class Kullanici
+    {
+        public int KullaniciID { get; set; }
+        public string KullaniciAdi { get; set; }
+        public string GercekAd { get; set; }
+        public string GercekSoyad { get; set; }
+        public string KullaniciAdres { get; set; }
+        public int TelefonNo { get; set; }
+
+    }
+}
