64b4761b0ec29db5da0b7af8795f5991898946ca
Kutay Sütçü <94748829+SutcuKutay@users.noreply.github.com>
2021-12-19 01:49:51 +0300

    Layout düzenlendi

---
+++ WebProgramlamaProjesi/WebProgramlamaProjesi/Controllers/KullaniciController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebProgramlamaProjesi.Controllers
+{
+    public class KullaniciController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+        public IActionResult Register()
+        {
+            return View();
+        }
+    }
+}
