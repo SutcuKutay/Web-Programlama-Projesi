aad2c77031926475aa8bb45d331cfd6d23a17152
Kutay Sütçü <94748829+SutcuKutay@users.noreply.github.com>
2021-12-19 23:19:52 +0300

    Kayıt Olma Sayfası tamamlandı

--- WebProgramlamaProjesi/WebProgramlamaProjesi/Controllers/KullaniciController.cs
+++ WebProgramlamaProjesi/WebProgramlamaProjesi/Controllers/KullaniciController.cs
@@ -3,12 +3,19 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WebProgramlamaProjesi.Data;
 using WebProgramlamaProjesi.Models;
 
 namespace WebProgramlamaProjesi.Controllers
 {
     public class KullaniciController : Controller
     {
+        private readonly ApplicationDbContext _context;
+
+        public KullaniciController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
         public IActionResult Login()
         {
             return View();
@@ -17,19 +24,29 @@ namespace WebProgramlamaProjesi.Controllers
         {
             return View();
         }
+        public IActionResult Basarili()
+        {
+            return View();
+        }
         [HttpPost]
         public IActionResult Kaydet(Kullanici user)
         {
             if (ModelState.IsValid)
             {
-                //veri tabani kayit vb.
-                TempData["msj"] = "Kayit islemi basarili.";
+                _context.Kullanici.Add(user);
+                _context.SaveChanges();
+                return RedirectToAction("Basarili");
             }
             else
             {
                 TempData["msj"] = "Basarisiz kayit.";
             }
             return RedirectToAction("Login");
+        }
+        [HttpPost]
+        public IActionResult Giris(Kullanici user)
+        {
+            return RedirectToAction("Basarili");
         }
     }
 }
---
+++ WebProgramlamaProjesi/WebProgramlamaProjesi/Data/Migrations/20211219184915_yarisiBurada.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebProgramlamaProjesi.Data.Migrations
+{
+    public partial class yarisiBurada : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "KullaniciAdi",
+                table: "Kullanici");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "GercekSoyad",
+                table: "Kullanici",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "GercekAd",
+                table: "Kullanici",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Email",
+                table: "Kullanici",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Sifre",
+                table: "Kullanici",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Email",
+                table: "Kullanici");
+
+            migrationBuilder.DropColumn(
+                name: "Sifre",
+                table: "Kullanici");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "GercekSoyad",
+                table: "Kullanici",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "GercekAd",
+                table: "Kullanici",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AddColumn<string>(
+                name: "KullaniciAdi",
+                table: "Kullanici",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+    }
+}
